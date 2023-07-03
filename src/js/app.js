7b62b1a2c6570d34bad905b7e299d57ab63d7f9c
Biggbada <yguenot@polepo.fr>
2023-07-03 15:35:26 +0200

    page search 50% - commit avant install swiper js

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,86 @@
-console.log("test");
-const meteoConceptToken =
-   "66c9531e068cf34224362c376d250bba937cdbd07f3a5f029a1fb44f7cd1b1b9";
+const directGeocodingApiRoot =
+   "https://api.openweathermap.org/geo/1.0/direct?q=";
+const reverseDirectGeocodingApiRoot =
+   "https://api.openweathermap.org/geo/1.0/reverse?";
+const oneCallApiRoot = "https://api.openweathermap.org/data/2.5/weather?";
+const directGeocodingApiKey = "db01a2cdeec12abd2aaf6de10f970d06";
+const oneCallApiKey = "c9eccf23c97be99196bb439d594c31e8";
+const citySearch = document.querySelector("#city-input");
+const citySubmit = document.querySelector("form");
 
-citySearch = document.querySelector("#city-input");
-const meteoConceptCityRoot = `./php/consume-meteo-concept.php?location=${citySearch.value}`;
-// const meteoConceptCityRoot = `http://api.meteo-concept.com/api/location/cities?token=${meteoConceptToken}&search=`;
-citySubmit = document.querySelector("form");
-console.log(citySearch);
-console.log(citySubmit);
-let searchedCity = "";
+citySearch.value = "dijon";
 
 citySubmit.addEventListener("submit", (event) => {
    event.preventDefault();
-   searchedCity = citySearch.value;
-   const meteoConceptCityQuery = meteoConceptCityRoot + searchedCity;
-   console.log("ok");
-   console.log("city: " + searchedCity);
-   console.log("root: " + meteoConceptCityRoot);
-   console.log("query: " + meteoConceptCityQuery);
-   fetch(meteoConceptCityQuery)
-      .then((response) => response.json)
-      .then((datas) => console.log(datas));
+   const cardContainers = document.querySelectorAll(".card-containers");
+   if (cardContainers) {
+   }
+   cardContainers.forEach((element) => {
+      element.remove();
+   });
+
+   const byCityQuery =
+      directGeocodingApiRoot +
+      citySearch.value +
+      "&limit=0&appid=" +
+      directGeocodingApiKey;
+
+   /**
+    *
+    *  FETCH
+    *    CITYBYQUERY
+    *
+    *    recherche la latitude et la longitude d'une ville
+    *    pour :
+    *       rechercher la meteo de la ville
+    */
+   fetch(byCityQuery)
+      .then((response) => {
+         return response.json();
+      })
+      .then((datas) => {
+         console.log(datas);
+         datas.forEach((element, i) => {
+            const byGeoCoordinatesQuery = `${oneCallApiRoot}lat=${element.lat}&lon=${element.lon}&units=metric&appid=${directGeocodingApiKey}`;
+            fetch(byGeoCoordinatesQuery)
+               .then((secondResponse) => {
+                  return secondResponse.json();
+               })
+               .then((donnees) => {
+                  const cityTemp = Math.round(donnees.main.temp);
+                  console.log(cityTemp);
+                  console.log(donnees);
+                  createFullElement("div", "#main-cards-container", {
+                     className: "flex-column card-containers",
+                     id: `card-containers${i}`,
+                  });
+                  createFullElement("div", `#card-containers${i}`, {
+                     className: "flex-row search-temp-weather",
+                     id: `temp-weather${i}`,
+                  });
+                  createFullElement("div", `#temp-weather${i}`, {
+                     className: "flex-column",
+                     textContent: `${cityTemp}°C`,
+                  });
+                  createFullElement("div", `#temp-weather${i}`, {
+                     className: "flex-column",
+                     id: `weather-icon-${i}`,
+                  });
+                  createFullElement("img", `#weather-icon-${i}`, {
+                     className: "flex-column",
+                     width: 30,
+                     height: 30,
+                     src: `./assets/Icones/standard/${donnees.weather[0].icon}.svg`,
+                  });
+                  createFullElement("div", `#card-containers${i}`, {
+                     className: "flex-row standard-texts results-city-title",
+                     textContent: element.name,
+                  });
+                  createFullElement("div", `#card-containers${i}`, {
+                     className: "flex-row standard-texts search-country-title",
+                     textContent: `${element.state} (${element.country})`,
+                  });
+               });
+         });
+      });
 });
---
+++ src/js/functions.js
@@ -0,0 +1,193 @@
+/**
+ *
+ *
+ *    there we define functions,
+ *       before exporting them in an appropriate js file
+ *
+ *
+ *
+ *
+ *
+ */
+// WE DEFINE FUNCTIONS
+//
+
+/**
+ *
+ * @param {string} shortDescription
+ *
+ * creates the container for wiki content
+ */
+
+/**
+ *
+ * @param {HTMLElement} wikiHtmlContent
+ * @returns the wikipedia short description
+ */
+function getWikiShortDescription(wikiHtmlContent) {
+   let shortDescription = wikiHtmlContent.querySelector(".shortdescription");
+   return shortDescription;
+}
+
+/**
+ *
+ * @param {JSON} json
+ * @returns wikiContent (html content)
+ */
+function getwikiContent(json) {
+   for (const property in json.parse.text) {
+      wikiContent = json.parse.text[property];
+      return wikiContent;
+   }
+}
+
+function serveWikiHtmlContent(wikiContent) {
+   let ghostDiv = document.createElement("div");
+   ghostDiv.innerHTML = wikiContent;
+   ghostDiv.id = "wiki-ghost-div";
+   return ghostDiv;
+}
+
+/** On ajoute le lien Wikipedia
+ *
+ * @param {JSON} datas
+ * @param {string} cleanedCityName
+ */
+function addWikiLink(datas, cleanedCityName) {
+   createFullElement("a", `#location-${datas.id} #wiki-div`, {
+      textContent: `See more on Wikipedia`,
+      href: `https://fr.wikipedia.org/wiki/${cleanedCityName}`,
+      target: "blank",
+   });
+}
+
+/**on scrap les images wikipedia
+ *
+ * @param {htmlElement} htmlElement
+ *
+ */
+function getWikiPictures(htmlElement) {
+   let wikiPictures = [];
+   wikiPictures = htmlElement.querySelectorAll("img");
+   return wikiPictures;
+}
+
+//****************************************************** */
+function getShortDescriptionFromWikipedia(wikiDatas) {
+   let returnData = [];
+   return returnData;
+}
+//****************************************************** */
+function createEnvironment() {
+   createFullElement("h1", "body", {
+      textContent: "Welcome",
+      id: "welcome-title",
+   });
+   createFullElement("div", "body", {
+      id: "main-container",
+   });
+
+   createFullElement("h2", "#main-container", {
+      textContent: "Search your location",
+   });
+   createFullElement("form", "#main-container", {
+      id: "search-form",
+      method: "post",
+      action: "none",
+   });
+   createFullElement("div", "#search-form", {
+      id: "inputs-container",
+   });
+   createFullElement("input", "#inputs-container", {
+      type: "text",
+      name: "weather-form",
+      id: "city-input",
+      placeholder: "City",
+   });
+   createFullElement("input", "#inputs-container", {
+      type: "text",
+      name: "weather-form",
+      id: "country-input",
+      placeholder: "Country",
+   });
+   createFullElement("button", "#inputs-container", {
+      type: "submit",
+      name: "weather-form",
+      id: "submit-button",
+      textContent: `Search`,
+   });
+   createFullElement("form", "#main-container", {
+      id: "random-search",
+      method: "post",
+      action: "none",
+   });
+   createFullElement("div", "#random-search", {
+      id: "random-search-container",
+   });
+   createFullElement("button", "#random-search", {
+      type: "submit",
+      name: "random-search-form",
+      id: "random-search-submit",
+      textContent: `Random search`,
+   });
+}
+
+//****************************************************** */
+function createCitiesContainers() {
+   createFullElement("h3", "#main-container", {
+      textContent: "Locations:",
+   });
+   createFullElement("ol", "#main-container", {
+      id: "suggestions",
+   });
+   createFullElement("div", "#suggestions", {
+      className: "cities-container",
+      id: "cities-container",
+   });
+}
+//****************************************************** */
+/**
+ *
+ * @param {string} tagName
+ * @param {string} source
+ * @param {string} properties
+ * @returns an html element with properties you want
+ */
+function createFullElement(tagName, source, properties) {
+   const element = document.createElement(tagName);
+   sourceElement = document.querySelector(source);
+   sourceElement.appendChild(element);
+
+   for (const property in properties) {
+      element[property] = properties[property];
+   }
+
+   return element;
+}
+/**
+ *
+ * @param {Array} picturesArray
+ * @param {JSON} datas
+ *
+ * displays each picture with his link
+ */
+function appendWikiPicsAndLinks(picturesArray, datas) {
+   picturesArray.forEach((element, i) => {
+      let wikiPicHref = element.parentElement;
+      let wikiPicLink = wikiPicHref.href?.replace(
+         "http://127.0.0.1:5501",
+         "https://en.wikipedia.org/"
+      );
+      createFullElement("a", `#location-${datas.id} #wiki-div`, {
+         href: wikiPicLink,
+         target: "blank",
+         id: `location-${datas.id}` + "-wikilink-" + [i],
+      });
+
+      createFullElement("img", `#location-${datas.id}` + "-wikilink-" + [i], {
+         src: element.src,
+         className: "wiki-pics",
+         id: `location-${datas.id}` + "-wikipic-" + [i],
+      });
+   });
+}
